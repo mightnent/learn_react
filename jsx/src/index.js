02147ddead945e0ddcb5dbe4abdcb92a449ec4f3
mightnent <mightnent@gmail.com>
2020-05-18 12:00:27 +0800

    basic jsx demo

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 
 // create a component
 const App = () => {
-    return <div>hello world</div>;
+    const buttonTxt = 'click'
+    const style = {backgroundColor:'red',color:'white'}
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            <button style={style}>
+                {buttonTxt}
+            </button>
+        </div>
+    );
 };
 
 //render it
