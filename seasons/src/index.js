55e43522ebc053c4925225e87f6a98ed3085ba9e
mightnent <mightnent@gmail.com>
2020-05-22 16:46:40 +0800

    init seasons app

---
+++ seasons/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        //only time with direct assigment
+        this.state = {lat:null,errorMessage:''};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat:position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage:err.message});
+            }
+        );
+    }
+
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        else if(!this.state.errorMessage && this.state.lat){
+            return <div>Lat: {this.state.lat}</div>;
+        } else{
+            return <div>Loading...</div>;
+        }
+    };
+};
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
