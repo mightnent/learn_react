92f09e57978a3461e9120a4aca2a75ad454e7bba
mightnent <mightnent@gmail.com>
2020-05-26 19:26:42 +0800

    css grid with react

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class ImageCard extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {spans : 0};
+        this.imageRef = React.createRef();
+    }
+
+    //only after render
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load,this.setSpan');
+    }
+
+    setSpan = () =>{
+        const height = this.imageRef.current.clientHeight;
+        //ceil will round up
+        const spans = Math.ceil(height/150)
+        this.setState({spans=spans})
+    }
+
+    render(){
+        //destructuring
+        const {description,urls} = this.props.image
+        return(
+            <div>
+                <img
+                    ref={this.imageRef}
+                    alt={description}
+                    src={urls.regular}
+                />
+            </div>
+        );
+    }
+}
+
+export default ImageCard;
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) =>{
 
     const images = props.images.map( (image) => {
-        return <img key = {image.id} alt={image.description} src={image.urls.regular} />
+        return <ImageCard key = {image.id} image={image}/>
     });
 
     return (
