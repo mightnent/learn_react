6772e775fc426df3ff09357563f26aed1cbb3aa0
mightnent <mightnent@gmail.com>
2020-05-28 21:53:21 +0800

    youtube api

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+const KEY = 'AIzaSyB_gCL8DGRcY02R9BhxAICuLmQQU_eItFw';
 
 class App extends React.Component{
+
+    state = {videos:[]};
+
+    onTermSubmit = async term =>{
+        const res = await youtube.get('./search',{
+            params:{
+                q:term,
+                part:'snippet',
+                maxResults:5,
+                key:KEY,
+            }
+        });
+
+        this.setState({videos:res.data.items});
+        
+    };
+
     render(){
         return(
-            <div className="ui container"><SearchBar/></div>
+            <div className="ui container">
+                <SearchBar onSubmit = {this.onTermSubmit}/>
+                {this.state.videos.length} videos found
+            </div>
         );
     }
 }
